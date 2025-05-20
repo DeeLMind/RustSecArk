711b2aca1deac3c098fbaf11b35ec770e99cbeb8
DeeLMind <deelmind@gmail.com>
2025-05-21 07:41:09 +0800

    bool

--- BasicSyntax/bool/src/main.rs
+++ BasicSyntax/bool/src/main.rs
@@ -1,10 +1,42 @@
 fn main() {
-    let true_value: bool = true;
-    let false_value: bool = false;
+    // Basic booleans
+    let t: bool = true;
+    let f: bool = false;
 
-    println!("true in binary:  {:08b}", true_value as u8);
-    println!("false in binary: {:08b}", false_value as u8);
-    
-    let result = format!("true in binary:  {:08b}\nfalse in binary: {:08b}\n", true_value as u8, false_value as u8);
-    println!("{}", result);
+    // Logical operators
+    let not_t = !t;
+    let and_tf = t && f;
+    let or_tf = t || f;
+
+    // Comparison results
+    let x = 7;
+    let y = 10;
+    let eq = x == y;
+    let neq = x != y;
+    let gt = x > y;
+    let lt = x < y;
+
+    // Bool to int (manual)
+    let bool_val = true;
+    let int_val = if bool_val { 1 } else { 0 };
+
+    // Output everything
+    println!("t: {}, f: {}", t, f);
+    println!("!t = {}", not_t);
+    println!("t && f = {}", and_tf);
+    println!("t || f = {}", or_tf);
+
+    println!("x == y: {}", eq);
+    println!("x != y: {}", neq);
+    println!("x > y:  {}", gt);
+    println!("x < y:  {}", lt);
+
+    println!("bool_val as int: {}", int_val);
+
+    // Use in condition
+    if t {
+        println!("Condition is true!");
+    } else {
+        println!("Condition is false!");
+    }
 }
