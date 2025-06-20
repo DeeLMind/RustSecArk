06d3bb84489b0368c7eb79853285252fd1120164
DeeLMind <deelmind@gmail.com>
2025-06-20 20:35:18 +0800

    extern_

--- BasicSyntax/slice_/src/main.rs
+++ BasicSyntax/slice_/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let arr = [10, 20, 30, 40, 50];
+
+    let a = &arr[..];      // 全部切片（等于 &arr）
+    let b = &arr[1..3];    // 从下标 1 到 2（不包括 3）
+    let c = &arr[..2];     // 从下标 0 到 1
+    let d = &arr[2..];     // 从下标 2 到结束
+
 }
