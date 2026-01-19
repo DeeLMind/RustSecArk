538fdc548a5b6e5820035cdb5f5ebe2494334dfc
DeeLMind <deelmind@gmail.com>
2026-01-19 13:32:34 +0800

    eBPF

--- TestMe/src/main.rs
+++ TestMe/src/main.rs
@@ -1,7 +1,24 @@
-use std::{fs::read, io::Read};
+use std::sync::{Arc, RwLock};
+use std::thread;
 
 fn main() {
-    let mut f = std::fs::File::open("path").unwrap();
-    let mut buf = [0u8; 1024];
-    f.read(&mut buf);
+    let data = Arc::new(RwLock::new(5));
+
+    // 多线程读
+    let r1 = Arc::clone(&data);
+    let reader = thread::spawn(move || {
+        let val = r1.read().unwrap();
+        println!("Read: {}", *val);
+    });
+
+    // 写
+    let w1 = Arc::clone(&data);
+    let writer = thread::spawn(move || {
+        let mut val = w1.write().unwrap();
+        *val += 1;
+        println!("Write done");
+    });
+
+    reader.join().unwrap();
+    writer.join().unwrap();
 }
